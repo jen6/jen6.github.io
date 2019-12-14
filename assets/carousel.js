60d5a00cb310a9f0deb44f3fba28554f33c2c879
Son Geon <fjen6b@gmail.com>
2019-12-15 03:01:54 +0900

    Site updated at 2019-12-14 18:01:53 UTC

---
+++ assets/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var clickEvent = false;
+  $('.carousel').carousel({
+    interval: 10999999999;
+  })
+});
+//$(window).load(function() {
+//  var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+//  var itemlength = $('#myCarousel .item').length;
+//  var triggerheight = Math.round(boxheight/itemlength+1);
+//});
